cc32a0deea9cd4025d425bed62a5a0223c661419
caleb <calebpaul21@gmail.com>
2020-10-15 12:31:09 -0700

    give credit

--- lib/main.dart
+++ lib/main.dart
@@ -98,6 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
+            Text('credit to /u/cairfrey for these plothooks'),
           ],
         ),
       ),
